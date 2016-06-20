51cd15b91ccf952a70c327b4469dbbbf004067ef
kon <idontwanttogoonthecart@gmail.com>
2016-06-20 19:48:05 +0300

    adding QUF

--- others/quick-find.js
+++ others/quick-find.js
@@ -1,3 +1,29 @@
-/**
- * Created by kon on 20.06.2016.
- */
+var QUF = (function () {
+  var proto = {
+    connected: function (a, b) {
+      return this.arr[a] === this.arr[b]
+    },
+    union: function (a, b) {
+      var aValue = this.arr[a];
+      var bValue = this.arr[b];
+
+      for (var i = 0; i < this.arr.length; i++) {
+        if (this.arr[i] === bValue) {
+          this.arr[i] = aValue;
+        }
+      }
+      return this.arr;
+    }
+  };
+
+  function QUF(arr) {
+    var obj = Object.create(proto);
+    obj.arr = arr;
+    return obj;
+  }
+
+  return QUF;
+}());
+
+
+console.log(QUF([1, 2, 1, 0, 0, 0, 1, 1, 2]).union(0,1))
