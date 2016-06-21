ed8bb4982bd6ff6459e29d192e4b3496e7048765
kon <idontwanttogoonthecart@gmail.com>
2016-06-21 19:28:59 +0300

    adding selection and insertion

--- sort/insertion.js
+++ sort/insertion.js
@@ -1,3 +1,34 @@
-/**
- * Created by kon on 21.06.2016.
- */
+var Insertion = (function () {
+  var proto = {
+    sort: function () {
+      for (var i = 0; i < this.arr.length; i++) {
+        for (var j = i; j >= 0; j--) {
+          if (this.less(j, j - 1)) {
+            this.swap(j, j - 1)
+          }
+        }
+      }
+      return this;
+    },
+    less: function (a, b) {
+      if (this.arr[a] < this.arr[b])
+        return true;
+      return false;
+    },
+    swap: function (a, b) {
+      var temp = this.arr[a];
+      this.arr[a] = this.arr[b];
+      this.arr[b] = temp;
+    }
+  }
+
+  function Insertion(arr) {
+    var obj = Object.create(proto)
+    obj.arr = arr;
+    return obj;
+  }
+
+  return Insertion;
+}())
+
+console.log(Insertion([3, 2, 5, 1, 10, 15, 1, 111, 4, 5, 119, 2, 3, 4]).sort().arr)
--- sort/selection.js
+++ sort/selection.js
@@ -1,3 +1,37 @@
-/**
- * Created by kon on 21.06.2016.
- */
+var Selection = (function () {
+  var proto = {
+    sort: function () {
+      var min;
+      for (var i = 0; i < this.arr.length; i++) {
+        min = i;
+        for (var j = i; j < this.arr.length; j++) {
+          if (this.less(j, min)) {
+            min = j;
+          }
+        }
+        this.swap(i, min)
+      }
+      return this;
+    },
+    less: function (a, b) {
+      if (this.arr[a] < this.arr[b])
+        return true;
+      return false;
+    },
+    swap: function (a, b) {
+      var temp = this.arr[a];
+      this.arr[a] = this.arr[b];
+      this.arr[b] = temp;
+    }
+  }
+
+  function Selection(arr) {
+    var obj = Object.create(proto)
+    obj.arr = arr;
+    return obj;
+  }
+
+  return Selection;
+}())
+
+console.log(Selection([3, 2, 5, 1, 10, 15, 1, 111, 4, 5, 119, 2, 3, 4]).sort().arr)
